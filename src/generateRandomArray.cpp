82a112b5cdf72ce5de68c6712430905bcff1477f
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-09-22 15:08:58 +0800

    Generate data in binary file for faster reading

--- src/generateRandomArray.cpp
+++ src/generateRandomArray.cpp
@@ -6,18 +6,23 @@
 #include <random>
 
 void generateRandomArray(int size, int lower_bound, int upper_bound, std::string filepath){
-    std::ofstream file(filepath);
+    std::ofstream file(filepath + ".txt");
+    std::ofstream binfile(filepath + ".dat", std::ios::binary);
 
-    if(!file){
-        std::cerr << "Couldn't open file " << filepath << std::endl;
-        return;
+    if(!file || !binfile){
+        throw new std::runtime_error("Couldn't open file for writing.");
     }
 
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dist(lower_bound, upper_bound);
 
+    std::vector<int> generated_values = std::vector<int>(size); 
+
     for (int i = 0; i < size; i++){
-        file << dist(gen) << " ";
+        generated_values[i] = dist(gen);
+        file << generated_values[i] << " ";
     }
+
+    binfile.write(reinterpret_cast<const char*>(generated_values.data()), size * sizeof(int));
 }
